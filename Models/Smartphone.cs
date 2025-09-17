499b88a5d3e690273a26ff72dcd8caebbdf7ffe5
wesleyDMP <wmdestudos@gmail.com>
2025-09-17 14:55:46 -0300

    Mais um Desafio

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,18 +2,39 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
+
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; }
+        public string Imei { get; set; }
+        public int Memoria { get; set; }
 
-        public Smartphone(string numero)
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+
+
+        }
+        /*Protected Smartphone(string modelo, string imei, int memoria)
+        {
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+        }
+        */
+        public void FichaTecnica()
+        {
+            Console.WriteLine($"Modelo: {Modelo}\nIMEI: {Imei}\nMemória: {Memoria}GB");
         }
 
         public void Ligar()
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine($"Ligando do ... {Numero}");  
         }
 
         public void ReceberLigacao()
